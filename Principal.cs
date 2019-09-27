a7fdd90f87edf1f46a19d58bd27ee4eb331aeef0
Alejandro10-28 <55776556+Alejandro10-28@users.noreply.github.com>
2019-09-27 15:09:25 -0700

    Add files via upload

---
+++ Principal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conversion_C
+{
+    class Principal
+    {
+
+        public void Convertir(int Totalcentavos,out int Pesos,out int Centavos)//Se crea un metodo para 
+            //la conversión donde totalcentavos es un parametro de entrada y los otros dos son de salida que son los que retornan
+            //valor.
+        {
+          //Se realizan las operaciónes de conversion.
+            Pesos = Totalcentavos / 100;
+           
+            Centavos = Totalcentavos % 100;
+
+        }
+        //Metodo de menú
+        public void Menu()
+        {
+            int Totalcentavos;
+            int Pesos = 0;
+            int Centavos = 0;
+            Console.WriteLine("Hola, bienvenido a la conversión de pesos");
+            Console.WriteLine("Ingrese valor a convertir:");
+            Totalcentavos = Convert.ToInt32(Console.ReadLine());//Ingresa el valor que se convertira
+            Convertir(Totalcentavos, out Pesos, out Centavos);//Se manda los parametros. Totalcentavo tomandolo como el
+            //parametro de entrada mientras que los otros seran de salida.
+            Console.WriteLine("El resultado es:" + Pesos +"."+ Centavos);//se imprimen los resultados
+            Console.ReadKey();
+                
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Conversion_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Se instancia la clase principal para mandar a llamar el menú.
+            Principal M = new Principal();
+            M.Menu();
+          
+                
+
+        }
+    }
+}
